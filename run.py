212bf16f1548a7456c421c8c98542b3a8ff4fd3f
FabricioGMV <fabriciog.veloso@hotmail.com>
2026-03-24 17:49:32 -0300

    Criacao das bibliotecas.

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from src.config.data_base import init_db
 from src.routes import init_routes
+from flask_jwt_extended import JWTManager
 
 def create_app():
     """
     Função que cria e configura a aplicação Flask.
     """
     app = Flask(__name__)
+    app.config["JWT_SECRET_KEY"] = "Teste@123"
+    jwt = JWTManager(app)
 
     init_db(app)
 
