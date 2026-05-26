d7b52dbec71a1345082fb51745e2f4a3e78062d8
Breno Goncalves Renzi Del Cacho <2403703@salas.aulas>
2026-05-26 18:36:38 -0300

    alterção do codigo

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\fabri\OneDrive\Documentos\Faculdade\Impacta\Noite\4sem\Full_Stack\GEMM\Hexagonal\.venv"
+set "VIRTUAL_ENV=C:\Users\2403703\Desktop\mini_mercado\Hexagonal\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
